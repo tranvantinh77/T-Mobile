279ad7a60ac7e56b52f4246a823393aa7c697345
tranvantinh117 <67227974+tranvantinh117@users.noreply.github.com>
2020-12-19 17:16:31 +0700

    layout shopping cart

--- app/src/main/java/com/vantinh/projectmobile/Fragment/ChiTietSPFragment.java
+++ app/src/main/java/com/vantinh/projectmobile/Fragment/ChiTietSPFragment.java
@@ -10,16 +10,27 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.vantinh.projectmobile.MainActivity;
 import com.vantinh.projectmobile.Model.SanPham;
 import com.vantinh.projectmobile.Model.ThuongHieu;
 import com.vantinh.projectmobile.R;
 
+import java.text.DecimalFormat;
+
 
 public class ChiTietSPFragment extends Fragment {
     public static final String TAG = ChiTietSPFragment.class.getName();
-    ImageView back_ct_sp;
-    TextView textct;
+    ImageView back_ct_sp, img_chi_tiet_sp;
+    TextView ten_chi_tiet_sp, gia_chi_tiet_sp, thong_so_sp, mo_ta_sp;
+
+    int id = 0;
+    String Tenchitiet = "";
+    Integer Giachitiet = 0;
+    String Hinhanhchitiet = "";
+    String Thongso = "";
+    String Motachitiet = "";
+    int Idsanpham = 0;
 
     View view;
     public ChiTietSPFragment() {
@@ -32,7 +43,11 @@ public class ChiTietSPFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_chi_tiet_s_p, container, false);
 
         back_ct_sp = view.findViewById(R.id.back_ct_sp);
-        textct = view.findViewById(R.id.textct);
+        img_chi_tiet_sp = view.findViewById(R.id.img_chi_tiet_sp);
+        ten_chi_tiet_sp = view.findViewById(R.id.ten_chi_tiet_sp);
+        gia_chi_tiet_sp = view.findViewById(R.id.gia_chi_tiet_sp);
+        thong_so_sp = view.findViewById(R.id.thong_so_sp);
+        mo_ta_sp = view.findViewById(R.id.mo_ta_sp);
 
         back_ct_sp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,9 +61,24 @@ public class ChiTietSPFragment extends Fragment {
 
         Bundle bundleReceive = getArguments();
         if (bundleReceive != null) {
-            SanPham sanPham = (SanPham) bundleReceive.get("dien_thoai");
+            SanPham sanPham = (SanPham) bundleReceive.getSerializable("dien_thoai");
             if (sanPham != null) {
-                textct.setText(sanPham.getTen());
+                id = sanPham.getID();
+                Tenchitiet = sanPham.getTen();
+                Giachitiet = sanPham.getGia();
+                Hinhanhchitiet = sanPham.getHinhanh();
+                Thongso = sanPham.getThongsokithuat();
+                Motachitiet = sanPham.getMota();
+                Idsanpham = sanPham.getIDsanpham();
+                ten_chi_tiet_sp.setText(Tenchitiet);
+                DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
+                gia_chi_tiet_sp.setText("Giá: " + decimalFormat.format(Giachitiet) + " Đ");
+                thong_so_sp.setText(Thongso);
+                mo_ta_sp.setText(Motachitiet);
+                Picasso.get()
+                        .load(Hinhanhchitiet)
+                        .into(img_chi_tiet_sp);
+
             }
         }
 
